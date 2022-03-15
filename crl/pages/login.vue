381a6e667f093ee5f5a798cfc638992fda7811d2
takeru0604twitter <takeru20030604@icloud.com>
2022-03-16 02:48:01 +0900

    a

---
+++ crl/pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button class="btn ">Button</button>
+        <button class="btn  btn-primary">Button</button>
+        <button class="btn btn-outline btn-secondary">Button</button>
+        <button class="btn btn-outline btn-accent">Button</button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
---
+++ crl/pages/profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+          <label class="swap">
+      <input type="checkbox"/>
+      <div class="swap-on">ON</div>
+      <div class="swap-off">OFF</div>
+    </label>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
