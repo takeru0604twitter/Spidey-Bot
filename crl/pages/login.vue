eb52b44bb858a6b5a9fb05b0dd78a9a3732b7b31
takeru0604twitter <takeru20030604@icloud.com>
2022-03-24 13:52:44 +0900

    Update login.vue

--- crl/pages/login.vue
+++ crl/pages/login.vue
@@ -1,25 +1,28 @@
 <template>
     <div>
-        <div class="form-control ">
-            <label class="label  flex justify-center">
+        <div class="form-control mt-56">
+            <label class="label flex justify-center">
                 <span class="label-text">>Your email</span>
             </label>
-            <label class="input-group flex justify-center">
+            <label class="input-group flex justify-center ">
                 <span>email</span>
                 <input type="text" class="input input-bordered">
             </label>
         </div>
-        <div class="form-control">
+        <div class="form-control mt-10">
             <label class="label flex justify-center">
                 <span class="label-text">Your password</span>
             </label>
             <label class="input-group flex justify-center">
                 <span>password</span>
-                <input type="text"  class="input input-bordered">
+                <input type="text" class="input input-bordered">
             </label>
         </div>
+        <div class="flex justify-center">
         <button class="btn">login</button>
         <a class="link">アカウントをお持ちでない方</a>
+        </div>
+
 
 
     </div>
