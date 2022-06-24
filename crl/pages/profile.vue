3dd358d7042adfd7665d663e4acee1e2ace8a088
takeru0604twitter <takeru20030604@icloud.com>
2022-06-25 02:37:18 +0900

    Update profile.vue

--- crl/pages/profile.vue
+++ crl/pages/profile.vue
@@ -1,11 +1,43 @@
 <template>
-    <div class="form-control ">
-      <div class="input-group">
-        <input type="text" placeholder="Search" class="input input-bordered ">
-        <button class="btn btn-square ">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-        </button>
-        {{ name }}
+    <div class="form-control">
+      <div class="ml-auto">
+        <div class="input-group">
+          <input type="text" placeholder="Search" class="input input-bordered ">
+          <button class="btn btn-square ">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 " fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+          </button>
+        </div> 
+      </div>
+      <div class="ml-48">
+        プレイヤー情報
+      </div>
+      <div class="display flex">
+        <div class="ml-48">
+          名前
+        </div>
+       <div class="ml-16">
+        クラン名
+        </div>
+       <div class="ml-32">
+        プレイヤータグ
+       </div>
+       <div class="ml-6">
+        トロフィー
+       </div>
+      </div>
+      <div class="display flex">
+        <div class="ml-48">
+          {{ name }}
+        </div>
+        <div class="ml-10">
+          {{clan}}
+        </div>
+      <div class="ml-10">
+        {{tag}}
+      </div>
+      <div class="ml-10">
+        {{trophy}}
+      </div>
       </div>
     </div>
 </template>
@@ -13,17 +45,37 @@
 <script lang="ts">
 import Vue from 'vue'
 
-interface Data {
-  name?: string
+interface Card{
+  name:string
+  id:number
+  level:number
+  starlevel:number
+  maxlevel:number
+  count:number
+  iconUrls:{
+    medius: string
+  }
 }
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFhNzM0NTYyLTg2OGQtNGE2OC05ZTdlLWZmZjM2YjU4NmE2MiIsImlhdCI6MTY0OTgzOTQyNCwic3ViIjoiZGV2ZWxvcGVyLzA5YzJmZmMxLTAyZjItMTI5My1hOWU5LTFmNWI5NTJjZjE5NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMDIuMTU3LjIzMC4xOTMiXSwidHlwZSI6ImNsaWVudCJ9XX0.CS3jKOgMr0BkWugZx9JbrQ8YfMb4UPNmMUhx1D93noZkMjvlj6uRe5WCculvODp2zdy7d0yy9ZjsYiNub00ziQ'
+interface Data {
+  name?: string
+  clan?: string
+  tag?: string
+  trophy?: string
+  deck:Card[]
+}
+
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImZkYmVhYzU4LWI5MjgtNDUzMS05ZTBjLTYzYWNjOGIyMDcwZCIsImlhdCI6MTY1NjA4NDk0MCwic3ViIjoiZGV2ZWxvcGVyLzA5YzJmZmMxLTAyZjItMTI5My1hOWU5LTFmNWI5NTJjZjE5NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIyMjMuMjUyLjkzLjE0Il0sInR5cGUiOiJjbGllbnQifV19.KcvNoqCJHuB65Sw-lecRNTnj-MHYXAScnv4TK5rGzMqcbR1RbmndHAakuAAEYmhSLuBDsK1IJEKYmwiLw4PZ6w'
 
 export default Vue.extend({
   name: 'IndexPage',
   data(): Data {
     return {
-      name: undefined
+      name: undefined,
+      clan: undefined,
+      tag: undefined,
+      trophy: undefined,
+      deck: []
     }
   },
   mounted() {
@@ -33,10 +85,15 @@ export default Vue.extend({
         Authorization: `Bearer ${token}`
       }
     })
-    .then(response => {
+    .then(response  => {
       console.log('response', response)
       this.name = response.data.name
+      this.tag = response.data.tag
+      this.trophy = response.data.trophies
+      this.clan = response.data.clan.name
     })
   }
+
+
 })
 </script>
